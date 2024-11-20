343f48a0152bdddd4c4369515d7bac44697068f7
mimii020 <imen.abidi@insat.ucar.tn>
2024-11-20 14:42:49 -0500

    added read an account feature

--- service/routes.py
+++ service/routes.py
@@ -1,6 +1,7 @@
 """
 Account Service
 
+
 This microservice handles the lifecycle of Accounts
 """
 # pylint: disable=unused-import
@@ -69,7 +70,13 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
-
+@app.route('/accounts/<int:account_id>', methods = ['GET'])
+def read_account(account_id):
+    app.logger.info('request to read an account with the id %s', account_id)
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f'account with id {account_id} could not be found')
+    return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
